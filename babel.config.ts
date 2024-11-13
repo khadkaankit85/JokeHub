286e73d9093dcd1bb1e3af4ed215d310be39b5e0
Angkit Khadka <khadkaankit85@gmail.com>
2024-11-12 20:13:02 -0500

    react native paper setup

---
+++ babel.config.ts
@@ -0,0 +1,13 @@
+import { ConfigAPI } from "@babel/core";
+
+module.exports = function (api: ConfigAPI) {
+  api.cache.forever(); // Caches indefinitely
+  return {
+    presets: ["babel-preset-expo"],
+    env: {
+      production: {
+        plugins: ["react-native-paper/babel"],
+      },
+    },
+  };
+};
